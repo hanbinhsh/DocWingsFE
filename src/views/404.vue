9bb05b389cabacbf9eef488978ebe08b432a3678
IceRinne <1024622242@qq.com>
2024-06-26 21:24:55 +0800

    404

---
+++ src/views/404.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="gray-bg">
+        <div class="middle-box text-center animated fadeInDown">
+            <h1>404</h1>
+            <h3 class="font-bold">页面未找到</h3>
+            <div class="error-desc">
+                看起来页面走丢了
+                <div class="form-inline m-t" role="form">
+                    <a href="/" class="btn btn-primary">返回首页</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
